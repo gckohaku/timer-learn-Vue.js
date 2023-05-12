6c9034caf50e1f1ba92cc6370bde87acdaa66cf7
gckohaku <cyanhairneko@gmail.com>
2023-05-12 13:35:59 +0900

    fix progress bar width2

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -88,7 +88,7 @@ const remainingTimePercent = computed((): number => {
 				</div>
 				<div class="relative mb-4">
 					<div class="overflow-hidden h-2 text-xs flex rounded bg-purple-200">
-						<div :style="'width' + remainingTimePercent + '%'" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-purple-500">
+						<div :style="'width:' + remainingTimePercent + '%'" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-purple-500">
 						</div>
 					</div>
 				</div>
